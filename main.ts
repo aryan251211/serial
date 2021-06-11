b5245c9d929598c9ead549a36d48b638472ad41c
aryan251211 <81881363+aryan251211@users.noreply.github.com>
2021-06-11 19:12:56 -0400

    I changed it to 5sec to 2sec

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 basic.forever(function () {
     for (let index = 0; index <= 1000; index++) {
         serial.writeValue("Current Temperature", input.temperature())
-        basic.pause(5000)
+        basic.pause(2000)
     }
 })
